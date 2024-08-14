a1b016c78ff56a88c2477eecf8ed7b40c17e2ea8
Ion Ghițun <ionghitun@gmail.com>
2024-08-14 14:15:44 +0300

    master - tema1

---
+++ resources/js/Layouts/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer className="w-full py-4 bg-blue-300 text-white">
+            <div className="flex container mx-auto">
+                <div>&copy; 2024 Practica. All rights reserved.</div>
+            </div>
+        </footer>
+    );
+}
